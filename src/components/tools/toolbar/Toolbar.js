bba776d305c00448eeeedbdc72570ff65f4d80b7
ygjack <ygjack414@hotmail.com>
2019-01-31 12:09:28 +0800

    update

--- src/components/tools/toolbar/Toolbar.js
+++ src/components/tools/toolbar/Toolbar.js
@@ -4,6 +4,17 @@ import MapDraggable from '@/components/MapDraggable'
 import { Icon, Tooltip } from 'antd'
 import styles from './MapToolbar.css'
 
+/**
+ * Because OptionsBar is highly related to active tools, 
+ * so can we define Toolbar like this:
+ * 
+ * <div>
+ *  <MapDraggableForToolbar />
+ *  <MapDraggableForOptionsBar />
+ * </div>
+ * 
+ * 即：反正都是  portal，不如这这里一起管理。潜在问题是减少了一些自定义的能力，如果外部希望 optionsBar 渲染到其他地方就不行了
+ */
 class Toolbar extends Component {
   clickHandler = toolKey => {
     this.props.onChange(toolKey)
