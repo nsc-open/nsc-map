25701aebaab53c35e20a0e8190991294841cf2fc
ygjack <ygjack414@hotmail.com>
2019-02-22 11:43:26 +0800

    add BasemapDropdown widget

---
+++ es5/core/BasemapManager.js
@@ -0,0 +1,91 @@
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+import { loadModules } from 'esri-module-loader';
+
+const removeBasemap = map => {
+  if (map.basemap) {
+    map.basemap.cancelLoad();
+    map.basemap.baseLayers.forEach(layer => map.remove(layer));
+  }
+}; // sample basemaps:
+// const BASEMAPS = [{
+//   id: 'statellite',
+//   label: '卫星影像',
+//   mapLayer: {
+//     urlTemplate: 'http://mt{subDomain}.google.cn/vt/lyrs=s&x={col}&y={row}&z={level}&s=Gali',
+//     subDomains: '0123'.split('')
+//   },
+//   annotationLayer: {
+//     urlTemplate: `http://t{subDomain}.tianditu.com/DataServer?tk=${TIANDITU_TOKEN}&T=cia_w&x={col}&y={row}&l={level}`,
+//     subDomains: '01234567'.split('')
+//   }
+// }]
+
+
+class BasemapManager {
+  constructor({
+    map,
+    basemaps = []
+  }) {
+    this.map = map;
+    this.annotationLayer = null;
+    this.basemaps = basemaps;
+    this.annotationVisible = false;
+  }
+
+  _removeBasemap() {
+    removeBasemap(this.map);
+  }
+
+  _setBasemap(basemap) {
+    this._removeBasemap();
+
+    loadModules(['esri/Basemap', 'esri/layers/WebTileLayer']).then(({
+      Basemap,
+      WebTileLayer
+    }) => {
+      const mapLayer = new WebTileLayer(basemap.mapLayer);
+      const annotationLayer = new WebTileLayer(_objectSpread({}, basemap.annotationLayer, {
+        visible: this.annotationVisible
+      }));
+      this.annotationLayer = annotationLayer;
+      this.map.basemap = new Basemap({
+        baseLayers: [mapLayer, annotationLayer]
+      });
+    });
+  }
+
+  showMap() {
+    const basemap = this.basemaps.find(b => b.id === 'map');
+
+    this._setBasemap(basemap);
+  }
+
+  showSatellite() {
+    const basemap = this.basemaps.find(b => b.id === 'statellite');
+
+    this._setBasemap(basemap);
+  }
+
+  showAnnotation() {
+    this.annotationVisible = true;
+
+    if (this.annotationLayer) {
+      this.annotationLayer.visible = true;
+    }
+  }
+
+  hideAnnotation() {
+    this.annotationVisible = false;
+
+    if (this.annotationLayer) {
+      this.annotationLayer.visible = false;
+    }
+  }
+
+}
+
+BasemapManager.removeBasemap = removeBasemap;
+export default BasemapManager;
--- examples/src/routes/IndexPage.js
+++ examples/src/routes/IndexPage.js
@@ -8,6 +8,9 @@ import Sketch from 'nsc-map/components/widgets/sketch/Sketch'
 import GroundObjectsLayer from 'nsc-map/components/GroundObjectsLayer'
 import GroundObject from 'nsc-map/components/GroundObject'
 import { polyline, point, polygon1, polygon2 } from 'mock/geometry-jsons'
+import { loadModules } from 'esri-module-loader' 
+import MapWidget from 'nsc-map/components/base/MapWidget'
+import BasemapDropdown from 'nsc-map/components/widgets/BasemapDropdown'
 
 
 const json1 = {"geometry":{"x":1.3358120593844913E7,"y":5310771.453513213,"spatialReference":{"wkid":102100}},"attributes":{"guid":"0ea639e8-a371-4949-b77b-2acdf5ad7e6b","name":"未命名","remark":"无","type":"point","movable":true,"editble":true,"visible":true},"symbol":{"color":[250,173,20,64],"outline":{"color":[250,173,20,255],"style":"esriSLSSolid","type":"esriSLS","width":1},"angle":0,"xoffset":0,"yoffset":0,"type":"esriSMS","size":10,"style":"esriSMSCircle"}}
@@ -41,11 +44,35 @@ export default class extends Component {
     }, 2000)
   }
 
+  onLoad = (map, view) => {
+    const TIANDITU_TOKEN = 'e2b08de0708e5136fff0fccaf21dd9bd'
+
+    const LAYERS = [/*{
+      key: 'google-map', 
+      urlTemplate: 'http://mt{subDomain}.google.cn/vt/lyrs=s&x={col}&y={row}&z={level}&s=Gali',
+      subDomains: '0123'.split(''),
+      title: '谷歌卫星地图'
+    }, */{
+      key: 'tianditu-map', 
+      urlTemplate: `http://t{subDomain}.tianditu.com/DataServer?tk=${TIANDITU_TOKEN}&T=vec_w&x={col}&y={row}&l={level}`,
+      subDomains: '01234567'.split(''),
+      title: '天地图电子地图'
+    },{
+      key: 'tianditu-annotation', 
+      urlTemplate: `http://t{subDomain}.tianditu.com/DataServer?tk=${TIANDITU_TOKEN}&T=cia_w&x={col}&y={row}&l={level}`,
+      subDomains: '01234567'.split(''),
+      title: '天地图注记'
+    }]
+
+    window.map = map
+    this.setState({ map: map })
+  }
+
   render () {
     const { selectedKeys, n } = this.state
     return (
       <Map
-        onLoad={(map, view)=> console.log('Map.onLoad()', map, view)}
+        onLoad={this.onLoad}
         loaderOptions={{ url: 'https://js.arcgis.com/4.8' }}
       >
         {/*
@@ -77,6 +104,11 @@ export default class extends Component {
        
 
        <Sketch />
+
+       <MapWidget draggable defaultPosition={{ x: 200, y: 200 }}>
+        {this.state.map && <BasemapDropdown map={this.state.map} />}
+       </MapWidget>
+
       </Map>
     )
   }
